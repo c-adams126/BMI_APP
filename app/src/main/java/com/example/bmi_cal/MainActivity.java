9e5e0ddf1e87e40d0faf0c9a0ee5359b75491a47
kalgo101 <44305744+kalgo101@users.noreply.github.com>
2022-01-10 13:53:03 -0500

    Start up

---
+++ app/src/main/java/com/example/bmi_cal/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.bmi_cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void change(View v){
+        TextView c = findViewById(R.id.theHello);
+        c.setText("it has been changed!!!");
+    }
+}
